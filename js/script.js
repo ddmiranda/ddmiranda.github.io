c66cbf14086ffb83e22a8dca874bca40112ede6a
Daniel Miranda <daniel.miranda@serpro.gov.br>
2018-07-02 01:45:26 -0300

    commit inicial

---
+++ js/script.js
@@ -0,0 +1,67 @@
+var inicio = Date.now()
+var inicioFracao = Date.now()
+var timer = 5500
+var umDecimo = timer / 10
+var umCentesimo = timer / 110
+var tempoDecorrido = Date.now() - inicio
+
+if (window.console) {
+  console.log('Hello Service Worker caching!');
+}
+
+function habilitar(nivel) {
+  for (var i = 1; i <= 10; i++) {
+    var elemento = document.getElementById(nivel + i)
+    if (elemento) {
+      elemento.className = "enable"
+    }
+  }
+}
+
+function ligar() {
+  var elemento = document.body
+  if (elemento) {
+    elemento.className = "ligado"
+  }
+
+  elemento = document.getElementById("container")
+  if (elemento) {
+    elemento.className = "ligado"
+  }
+}
+
+function habilitarTudo() {
+  habilitar('a')
+  habilitar('b')
+}
+
+
+function desabilitar(nivel, tempo, fracao) {
+  habilitar(nivel)
+  var decorrido = Date.now() - tempo
+  var quantidade = Math.trunc(decorrido / fracao)
+  for (var i = 1; i <= quantidade; i++) {
+    var elemento = document.getElementById(nivel + i)
+    if (elemento) {
+      elemento.className = "disable"
+    }
+  }
+  return quantidade * fracao + tempo;
+}
+
+var intervalo;
+
+function atualizar() {
+  
+  var tempo1 = desabilitar("a", inicio, umDecimo)
+  desabilitar("b", tempo1, umCentesimo)
+  if ((Date.now() - inicio - 200) > timer) {
+    habilitarTudo()
+    ligar()
+    window.setTimeout(function(){window.clearInterval(intervalo)}, 1)
+  }
+}
+
+intervalo = window.setInterval(function () {
+  atualizar()
+}, umCentesimo)
